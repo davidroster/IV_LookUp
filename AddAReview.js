66fce5b0a4c1bd9bce4a08d233e9e610fa0e3215
ahsansyed97 <ahsan1223@gmail.com>
2019-06-10 19:00:19 -0700

    Update AddAReview.js

--- AddAReview.js
+++ AddAReview.js
@@ -1,7 +1,16 @@
+addresses = ['6694 Picasso', '6710 Trigo', '6753 Abrego', '6763 Abrego', '776 Camino del Sur', '6519 Cervantes', '6761 Del Playa', '750 Embarcadero del Mar', '760 Embarcadero del Mar', '755 Embarcadero del Norte']
+var select = document.getElementById("locationInput");
+var inner_string = "";
+for(var i = 0; i < addresses.length; i++) {
+    inner_string += "<option>";
+    inner_string += addresses[i];
+    inner_string += "</option>";
+}
+select.innerHTML = inner_string;
+
 function callAddReview() {
   var address = $("input[name='locationInput']:selected").val();
-  var rating_string = $("input[name='rating']:checked").val();
-  rating = parseInt(rating_string, 10)
+  var rating = $("input[name='rating']:checked").val();
   var name = $("input[name='nameInput']").val();
   var date = $("input[name='dateInput']").val()
   var review = $("input[name='reviewInput']").val();
