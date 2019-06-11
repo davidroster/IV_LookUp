488dfd5ccb99ad03a7fe3f2b1e81684dc75cdfa3
ahsansyed97 <ahsan1223@gmail.com>
2019-06-10 18:25:55 -0700

    Update AddAReview.js

--- AddAReview.js
+++ AddAReview.js
@@ -1,7 +1,7 @@
 function callAddReview() {
   var address = $("input[name='locationInput']:selected").val();
   var rating_string = $("input[name='rating']:checked").val();
-  rating = ParseInt(rating_string, 10)
+  rating = parseInt(rating_string, 10)
   var name = $("input[name='nameInput']").val();
   var date = $("input[name='dateInput']").val()
   var review = $("input[name='reviewInput']").val();
